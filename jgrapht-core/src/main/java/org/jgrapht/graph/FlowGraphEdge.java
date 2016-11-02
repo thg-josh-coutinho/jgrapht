aaa5e44ee204d8e9eefa4361deb99d772e90d366
Joshua Coutinho <josh.coutinho@thehutgroup.com>
2016-11-02 14:39:11 +0000

    Added to string method

--- jgrapht-core/src/main/java/org/jgrapht/graph/FlowGraphEdge.java
+++ jgrapht-core/src/main/java/org/jgrapht/graph/FlowGraphEdge.java
@@ -1,8 +1,15 @@
 package org.jgrapht.graph;
 
+import java.util.*;
 import org.jgrapht.graph.DefaultWeightedEdge;
 
 public class FlowGraphEdge extends DefaultWeightedEdge
 {
+
+    public String toString()
+    {
+	return String.format("%s - %f - %s", getSource(), weight, getTarget());
+    }
+
 
 }
